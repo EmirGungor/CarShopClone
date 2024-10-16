ded61725ddd0df94a3f0e6b70e6d44c7944a06e0
Emir <96845612+EmirGungor@users.noreply.github.com>
2024-10-16 22:07:29 +0300

    car models added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 // eslint-disable-next-line no-unused-vars
-import * as React from 'react';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 const Sidebar = () => {
   return (
     <div className="Sidebar">
       <FormGroup>
-        <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-        <FormControlLabel required control={<Checkbox />} label="Required" />
-        <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+        <p>Marka</p>
+        <FormControlLabel control={<Checkbox />} label="Tümü (69)" />
+        <FormControlLabel control={<Checkbox />} label="AUDI (2)" />
+        <FormControlLabel control={<Checkbox />} label="BMW (5)" />
+        <FormControlLabel control={<Checkbox />} label="CITROEN (3)" />
+        <FormControlLabel control={<Checkbox />} label="DACIA (12)" />
+        <FormControlLabel control={<Checkbox />} label="DS AUTOMOBILES(1)" />
+        <FormControlLabel control={<Checkbox />} label="FIAT (6)" />
+        <FormControlLabel control={<Checkbox />} label="FORD (3)" />
       </FormGroup>
     </div>
   );

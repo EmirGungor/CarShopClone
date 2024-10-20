94c38f323dc693e4ce1dbf3a9a22274c867a3b83
EmirGungor <emirgungor07@hotmail.com>
2024-10-20 23:43:58 +0300

    Today's Work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import CarList from "./components/CarList";
+/* eslint-disable no-unused-vars */
 import Navbar from "./components/navbar";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
@@ -11,6 +11,7 @@ import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Section4 from "./components/Section4";
 import CarCard from "./components/CarCard";
+import { Box } from "@mui/material";
 
 function App() {
   return (
@@ -18,126 +19,82 @@ function App() {
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
       <Navbar />
-      <div style={{ display: "flex", flexGrow: 1, marginTop: "20px" }}>
+      <div
+        style={{
+          display: "flex",
+          flexGrow: 1,
+          marginTop: "20px",
+        }}
+      >
         <Sidebar />
-        {/* <div>
-          <CarList
-            Title={"Renault Megane"}
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <CarCard
             image={img1}
-            Price={"849.200"}
-            Engine={"1.3 TCe Joy EDC"}
-            ModalYear={"2002"}
-            Km={"204.600"}
-            Transmisson={"Otomatik"}
+            title={"RENAULT MEGANE"}
+            engine={"1.3 TCe Joy EDC"}
+            km={"38.991"}
+            model={"2022"}
+            transmission={"Otomatik"}
+            fuel={"Benzin"}
+            price={"849.400"}
           />
-          <CarList
-            Title={"Hyundai Getz"}
+          <CarCard
             image={img3}
-            Price={"430.100"}
-            Engine={"1.4 DOHC"}
-            ModalYear={"2002"}
-            Km={"350.300"}
-            Transmisson={"Manuel"}
+            title={"Hyundai Getz"}
+            engine={"1.4 DOHC"}
+            km={"350.300"}
+            model={"2009"}
+            transmission={"Manuel"}
+            fuel={"Gas"}
+            price={"430.100"}
           />
-          <CarList
-            Title={"TOYOTA C-HR"}
+          <CarCard
             image={img4}
-            Price={"1.170.400"}
-            Engine={"1.8 Hybrid 4x2 Flame e-CVT"}
-            ModalYear={"2021"}
-            Km={"13.117"}
-            Transmisson={"Otomatik"}
+            title={"TOYOTA C-HR"}
+            engine={"1.8 Hybrid 4x2 Flame e-CVT"}
+            km={"13.117"}
+            model={"2021"}
+            transmission={"Otomatik"}
+            fuel={"Hybrid"}
+            price={"1.170.400"}
           />
-          <CarList
-            Title={"Renault Megane"}
+          <CarCard
             image={img1}
-            Price={"849.200"}
-            Engine={"1.3 TCe Joy EDC"}
-            ModalYear={"2002"}
-            Km={"204.600"}
-            Transmisson={"Otomatik"}
+            title={"RENAULT MEGANE"}
+            engine={"1.3 TCe Joy EDC"}
+            km={"38.991"}
+            model={"2022"}
+            transmission={"Otomatik"}
+            fuel={"Benzin"}
+            price={"849.400"}
           />
-          <CarList
-            Title={"Hyundai Getz"}
-            image={img2}
-            Price={"430.100"}
-            Engine={"1.4 DOHC"}
-            ModalYear={"2002"}
-            Km={"350.300"}
-            Transmisson={"Manuel"}
+          <CarCard
+            image={img3}
+            title={"Hyundai Getz"}
+            engine={"1.4 DOHC"}
+            km={"350.300"}
+            model={"2002"}
+            transmission={"Manuel"}
+            fuel={"Gas"}
+            price={"430.100"}
           />
-          <CarList
-            Title={"TOYOTA C-HR"}
+          <CarCard
             image={img4}
-            Price={"1.170.400"}
-            Engine={"1.8 Hybrid 4x2 Flame e-CVT"}
-            ModalYear={"2021"}
-            Km={"13.117"}
-            Transmisson={"Otomatik"}
+            title={"TOYOTA C-HR"}
+            engine={"1.8 Hybrid 4x2 Flame e-CVT"}
+            km={"13.117"}
+            model={"2021"}
+            transmission={"Otomatik"}
+            fuel={"Hybrid"}
+            price={"1.170.400"}
           />
-          <CarList
-            Title={"Renault Megane"}
-            image={img1}
-            Price={"849.200"}
-            Engine={"1.3 TCe Joy EDC"}
-            ModalYear={"2002"}
-            Km={"204.600"}
-            Transmisson={"Otomatik"}
-          />
-          <CarList
-            Title={"Hyundai Getz"}
-            image={img2}
-            Price={"430.100"}
-            Engine={"1.4 DOHC"}
-            ModalYear={"2002"}
-            Km={"350.300"}
-            Transmisson={"Manuel"}
-          />
-          <CarList
-            Title={"TOYOTA C-HR"}
-            image={img4}
-            Price={"1.170.400"}
-            Engine={"1.8 Hybrid 4x2 Flame e-CVT"}
-            ModalYear={"2021"}
-            Km={"13.117"}
-            Transmisson={"Otomatik"}
-          />
-          <CarList
-            Title={"Renault Megane"}
-            image={img1}
-            Price={"849.200"}
-            Engine={"1.3 TCe Joy EDC"}
-            ModalYear={"2002"}
-            Km={"204.600"}
-            Transmisson={"Otomatik"}
-          />
-          <CarList
-            Title={"Hyundai Getz"}
-            image={img2}
-            Price={"430.100"}
-            Engine={"1.4 DOHC"}
-            ModalYear={"2002"}
-            Km={"350.300"}
-            Transmisson={"Manuel"}
-          />
-          <CarList
-            Title={"TOYOTA C-HR"}
-            image={img4}
-            Price={"1.170.400"}
-            Engine={"1.8 Hybrid 4x2 Flame e-CVT"}
-            ModalYear={"2021"}
-            Km={"13.117"}
-            Transmisson={"Otomatik"}
-          />
-        </div> */}
-        <div>
-          <CarCard/>
-        </div>
+        </Box>
       </div>
-      <Section1/>
-      <Section2/>
-      <Section3/>
-      <Section4/>
+
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
       <Footer />
     </div>
   );
--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import {
@@ -8,42 +9,128 @@ import {
   Grid,
   Button,
   Box,
+  IconButton,
 } from "@mui/material";
-import img1 from "../assets/car1.jpg";
+import InfoIcon from "@mui/icons-material/Info";
 
-const CarCard = () => {
+const CarCard = ({
+  image,
+  title,
+  engine,
+  model,
+  km,
+  transmission,
+  fuel,
+  price,
+}) => {
   return (
-    <Card sx={{ display: "flex", width: 1300, padding: 2 }}>
+    <Card
+      sx={{
+        display: "flex",
+        width: 1300,
+        height: "9rem",
+        padding: 1,
+        marginLeft: "1rem",
+      }}
+    >
       {/* Resim Bölümü */}
       <CardMedia
         component="img"
-        sx={{ width: 206, height: 130, objectFit: "contain", borderRadius:'30px' }}
-        image={img1}
-        alt="Renault Megane"
-        
+        sx={{ width: 206, height: 130, borderRadius: "5px" }}
+        image={image}
+        alt={title}
       />
 
       {/* İçerik Bölümü */}
       <Box sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
         <CardContent sx={{ flex: "1 0 auto" }}>
-          <Grid container spacing={2}>
+          <Grid container spacing={0} sx={{ height: "100%" }}>
             {/* Sol bölüm: Araba bilgileri */}
-            <Grid item xs={8}>
-              <Typography sx={{color:'#317B52'}} variant="h6">RENAULT MEGANE</Typography>
-              <Typography variant="body2" color="textSecondary" gutterBottom>
-                1.3 TCe Joy EDC
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                2022 • 38.991 KM • Otomatik • Benzin • KDV %20
-              </Typography>
+            <Grid
+              item
+              xs={8}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "100%",
+              }}
+            >
+              <Box sx={{ flexGrow: 1 }}>
+                <Typography sx={{ color: "#317B52" }} variant="h6">
+                  {title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" gutterBottom>
+                  {engine}
+                </Typography>
+              </Box>
+              <Box sx={{ display: "flex", gap: 2 }}>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ mt: "auto" }}
+                >
+                  {model}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ mt: "auto" }}
+                >
+                  {km} KM
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ mt: "auto" }}
+                >
+                  {transmission}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ mt: "auto" }}
+                >
+                  {fuel}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  sx={{ mt: "auto" }}
+                >
+                  KDV %20
+                </Typography>
+              </Box>
             </Grid>
 
             {/* Sağ bölüm: Fiyat ve buton */}
             <Grid item xs={4} sx={{ textAlign: "right" }}>
-              <Typography variant="h6" color="textPrimary">
-                849.400 TL
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Typography variant="body2" color="textSecondary">
+                  Satış Fiyatı / TL
+                </Typography>
+                <IconButton aria-label="info" size="small">
+                  <InfoIcon fontSize="small" />
+                </IconButton>
+              </Box>
+              <Typography
+                variant="h6"
+                color="textPrimary"
+                sx={{ fontWeight: "700" }}
+              >
+                {price}
               </Typography>
-              <Button variant="contained" color="success" sx={{ mt: 2 }}>
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ mt: 2, borderRadius: "30px" }}
+              >
                 Hemen Al
               </Button>
             </Grid>

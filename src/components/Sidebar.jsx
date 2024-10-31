d70a4eea17f9c1c7c725bea2e00ea5bdcef2b57f
Emir <emirgungor07@hotmail.com>
2024-10-31 15:47:49 +0300

    Added filter

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,63 +1,92 @@
-// eslint-disable-next-line no-unused-vars
-import * as React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { TextField } from "@mui/material";
+import { TextField, Box } from "@mui/material";
 
-const Sidebar = () => {
+const Sidebar = ({ onFilterChange }) => {
+  const brands = ["RENAULT MEGANE", "FORD TOURNEO COURIER", "RENAULT CLIO"];
+  const transmissions = ["Otomatik", "Manuel"];
+  const fuelTypes = ["Benzin", "Dizel"];
+
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [selectedTransmissions, setSelectedTransmissions] = useState([]);
+  const [selectedFuelTypes, setSelectedFuelTypes] = useState([]);
+
+  const handleCheckboxChange = (category, value, setState) => {
+    setState((prev) =>
+      prev.includes(value)
+        ? prev.filter((item) => item !== value) // Seçiliyse kaldır
+        : [...prev, value] // Seçili değilse ekle
+    );
+  };
+
+  useEffect(() => {
+    onFilterChange({
+      brands: selectedBrands,
+      transmissions: selectedTransmissions,
+      fuelTypes: selectedFuelTypes,
+    });
+  }, [selectedBrands, selectedTransmissions, selectedFuelTypes, onFilterChange]);
+
   return (
     <div className="Sidebar">
-      <FormGroup className="brand">
+      {/* Marka Seçenekleri */}
+      <FormGroup>
         <p>Marka</p>
-        <FormControlLabel control={<Checkbox />} label="Tümü (69)" />
-        <FormControlLabel control={<Checkbox />} label="AUDI (2)" />
-        <FormControlLabel control={<Checkbox />} label="BMW (5)" />
-        <FormControlLabel control={<Checkbox />} label="CITROEN (3)" />
-        <FormControlLabel control={<Checkbox />} label="DACIA (12)" />
-        <FormControlLabel control={<Checkbox />} label="DS AUTOMOBILES(1)" />
-        <FormControlLabel control={<Checkbox />} label="FIAT (6)" />
-        <FormControlLabel control={<Checkbox />} label="FORD (3)" />
+        {brands.map((brand) => (
+          <FormControlLabel
+            key={brand}
+            control={
+              <Checkbox
+                checked={selectedBrands.includes(brand)}
+                onChange={() =>
+                  handleCheckboxChange("brands", brand, setSelectedBrands)
+                }
+              />
+            }
+            label={brand}
+          />
+        ))}
       </FormGroup>
+
+      {/* Vites Tipi */}
       <FormGroup>
         <p>Vites Tipi</p>
-        <FormControlLabel control={<Checkbox />} label="Tümü" />
-        <FormControlLabel control={<Checkbox />} label="Manuel" />
-        <FormControlLabel control={<Checkbox />} label="Otomatik" />
+        {transmissions.map((transmission) => (
+          <FormControlLabel
+            key={transmission}
+            control={
+              <Checkbox
+                checked={selectedTransmissions.includes(transmission)}
+                onChange={() =>
+                  handleCheckboxChange("transmissions", transmission, setSelectedTransmissions)
+                }
+              />
+            }
+            label={transmission}
+          />
+        ))}
       </FormGroup>
+
+      {/* Yakıt Tipi */}
       <FormGroup>
         <p>Yakıt Tipi</p>
-        <FormControlLabel control={<Checkbox />} label="Tümü" />
-        <FormControlLabel control={<Checkbox />} label="Manuel" />
-        <FormControlLabel control={<Checkbox />} label="Otomatik" />
-      </FormGroup>
-      <br />
-      <p>Fiyat</p>
-      <div className="textField">
-        <TextField id="outlined-basic" label="Min" variant="outlined" />
-        <TextField id="outlined-basic" label="Max" variant="outlined" />
-      </div>
-      <p>Model Yılı</p>
-      <div className="textField">
-        <TextField id="outlined-basic" label="1998" variant="outlined" />
-        <TextField id="outlined-basic" label="2024" variant="outlined" />
-      </div>
-      <p>Km</p>
-      <div className="textField">
-        <TextField id="outlined-basic" label="0" variant="outlined" />
-        <TextField id="outlined-basic" label="400.000" variant="outlined" />
-      </div>
-      <br />
-      <FormGroup className="brand">
-        <p>Renk</p>
-        <FormControlLabel control={<Checkbox />} label="Tümü (69)" />
-        <FormControlLabel control={<Checkbox />} label="AUDI (2)" />
-        <FormControlLabel control={<Checkbox />} label="BMW (5)" />
-        <FormControlLabel control={<Checkbox />} label="CITROEN (3)" />
-        <FormControlLabel control={<Checkbox />} label="DACIA (12)" />
-        <FormControlLabel control={<Checkbox />} label="DS AUTOMOBILES(1)" />
-        <FormControlLabel control={<Checkbox />} label="FIAT (6)" />
-        <FormControlLabel control={<Checkbox />} label="FORD (3)" />
+        {fuelTypes.map((fuel) => (
+          <FormControlLabel
+            key={fuel}
+            control={
+              <Checkbox
+                checked={selectedFuelTypes.includes(fuel)}
+                onChange={() =>
+                  handleCheckboxChange("fuelTypes", fuel, setSelectedFuelTypes)
+                }
+              />
+            }
+            label={fuel}
+          />
+        ))}
       </FormGroup>
     </div>
   );
